e93398d100a16bf6d6b674fc81029835538e4f71
kiminpyo <rladlsvy2448@naver.com>
2022-02-23 01:26:09 +0900

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://inpyo:whoistheking9!@boilerplate.apclh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log('error'))
+
+
+app.get('/', (req,res) => res.send('Hello World'))
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
