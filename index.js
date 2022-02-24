6fb0c4a2a5fca060d8517a4a42045e700b2c2a75
kiminpyo <rladlsvy2448@naver.com>
2022-02-24 19:13:18 +0900

    third-commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User} = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -12,14 +15,14 @@ app.use(bodyParser.json());
 
 //mongodb 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://inpyo:whoistheking9!@boilerplate.apclh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected'))
-.catch(err => console.log('error'))
+.catch(err => console.log('error', config.mongoURI))
 
 
 
 
-app.get('/', (req,res) => res.send('Hello World'))
+app.get('/', (req,res) => res.send('Hello World gdgd'))
 
 
 app.post('/register',(req,res) => {
