849d6d28b38fe7edde702d89f7b40aa07b627a42
kiminpyo <rladlsvy2448@naver.com>
2022-02-27 05:05:45 +0900

    로그인 페이지 구현 중

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,8 +1,54 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useDispatch} from 'react-redux'
+import { loginUser } from '../../../_actions/user_action';
 function LoginPage() {
+  const dispatch = useDispatch;
+
+  const [Email, setEmail] = useState("")
+  const [Password,setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const onPasswordHandler = (event) =>{
+    setPassword(event.target.value)
+  }
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    // 안해주면 페이지가 리프레쉬된다.
+
+    console.log('Email', Email)
+    console.log('Password',Password)
+
+    //state 에 갔다.
+    let body ={
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+
+    
+
+    
+  }
+
   return (
-    <div>LoginPage</div>
+    <div  style={{display: 'flex', justifyContent: 'center', alignItems:'center'
+    ,width: '100%', height: '100vh'}}>
+      <form style={{display:'flex', flexDirection: 'column'}}
+      onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" valu={Password} onChange={onPasswordHandler} />
+        <br />
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
   )
 }
 
